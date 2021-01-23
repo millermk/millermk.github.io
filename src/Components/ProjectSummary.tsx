6d59341ce3d5a0cce2305df1592dfbc8f9d17897
Matthew Miller <millermk@outlook.com>
2021-01-23 16:03:05 -0600

    Add ability to associate a video with a project

--- src/Components/ProjectSummary.tsx
+++ src/Components/ProjectSummary.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Row } from 'reactstrap';
+import './ProjectSummary.css';
 import Project from '../Models/Project';
+import YoutubeEmbed from './YoutubeEmbed';
 
 export default function ProjectSummary(props: {project: Project}) {
     return (
         <Row className="bg-light mb-2 py-2 px-4">
             <h3>{props.project.name}</h3>
             <p>{props.project.description}</p>
+            <div className="video-container-limited-width">
+                {props.project.youtubeVideoID ? <YoutubeEmbed videoID={props.project.youtubeVideoID}/> : null}
+            </div>
         </Row>
     );
 }
--- src/Models/Project.ts
+++ src/Models/Project.ts
@@ -1,4 +1,5 @@
 export default interface Project {
     name: string;
     description: string;
+    youtubeVideoID?: string;
 }
