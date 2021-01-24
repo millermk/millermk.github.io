10510dccc605957b136802fe502f753dfb7e10e0
Matthew Miller <millermk@outlook.com>
2021-01-23 18:50:28 -0600

    Add CV Download to NavBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './NavBar.css'
 import { NavLink as RouterNavLink} from 'react-router-dom';
 import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 
@@ -17,12 +18,13 @@ export default function NavBar() {
                     <NavbarBrand className="mr-2"><RouterNavLink to="/">Matthew Miller</RouterNavLink></NavbarBrand>
                     <NavbarToggler onClick={toggle} />
                     <Collapse isOpen={isOpen} navbar>
-                        <Nav navbar>
+                        <Nav navbar className="mr-auto">
                             <NavItem><NavLink active={true}><RouterNavLink activeClassName="link-active" onClick={close} to="/projects">Projects</RouterNavLink></NavLink></NavItem>
                             <NavItem><NavLink active={true}><RouterNavLink activeClassName="link-active" onClick={close} to="/publications">Publications</RouterNavLink></NavLink></NavItem>
                             <NavItem><NavLink active={true}><RouterNavLink activeClassName="link-active" onClick={close} to="/experience">Experience</RouterNavLink></NavLink></NavItem>
                             <NavItem><NavLink active={true}><RouterNavLink activeClassName="link-active" onClick={close} to="/service">Service</RouterNavLink></NavLink></NavItem>
                         </Nav>
+                        <NavLink href={(process.env.PUBLIC_URL || "/") + 'matthew-miller-cv.pdf'} className="nav-link-download" download>My CV (pdf)</NavLink>
                     </Collapse>
                 </Navbar>
             </Container>
