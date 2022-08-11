5f5f612975529d4839daf214f75da3535845a102
Matthew Miller <millermk@outlook.com>
2022-08-11 00:15:30 -0600

    Add strict mode to react

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,4 @@ import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(<React.StrictMode><App /></React.StrictMode>);
